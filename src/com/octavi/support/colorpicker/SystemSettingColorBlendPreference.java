ZenX-OS/android_vendor_support

support: Add ColorBlendPreference

src/com/zenx/support/colorpicker/SystemSettingColorBlendPreference.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2018 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.zenx.support.colorpicker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.zenx.support.colorpicker.ColorBlendPreference;
+import com.zenx.support.preferences.SystemSettingsStore;
+public class SystemSettingColorBlendPreference extends ColorBlendPreference {
+
+    public SystemSettingColorBlendPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorBlendPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorBlendPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}

Octavi-OS/platform_vendor_support

src/com/octavi/support/colorpicker/SystemSettingColorBlendPreference.java
@@ -1,0 +1,40 @@
+/*
+ * Copyright (C) 2018 Android Ice Cold Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.octavi.support.colorpicker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+
+import com.octavi.support.colorpicker.ColorBlendPreference;
+import com.octavi.support.preferences.SystemSettingsStore;
+public class SystemSettingColorBlendPreference extends ColorBlendPreference {
+
+    public SystemSettingColorBlendPreference(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorBlendPreference(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+
+    public SystemSettingColorBlendPreference(Context context) {
+        super(context);
+        setPreferenceDataStore(new SystemSettingsStore(context.getContentResolver()));
+    }
+}