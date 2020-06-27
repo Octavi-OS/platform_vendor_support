Havoc-OS/android_vendor_support

support: ColorPicker: Change color picker reset icon
Change-Id: I49d17a9aff4c4d99207e02b8da677e0d47188547

src/com/havoc/support/colorpicker/ColorPickerPreference.java
@@ -35,6 +35,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.havoc.support.R;
+
 /**
  * A preference type that allows a user to choose a time
  *
@@ -152,7 +154,7 @@ public class ColorPickerPreference extends Preference implements
 
         widgetFrameView.addView(defView);
         widgetFrameView.setMinimumWidth(0);
-        defView.setBackground(getContext().getDrawable(android.R.drawable.ic_menu_revert));
+        defView.setBackground(getContext().getDrawable(R.drawable.ic_settings_backup_restore));
         defView.setTag("default");
         defView.setOnClickListener(new View.OnClickListener() {
             @Override

Octavi-OS/platform_vendor_support

src/com/octavi/support/colorpicker/ColorPickerPreference.java
@@ -35,6 +35,8 @@ import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 
+import com.zenx.support.R;
+
 /**
  * A preference type that allows a user to choose a time
  *
@@ -152,7 +154,7 @@ public class ColorPickerPreference extends Preference implements
 
         widgetFrameView.addView(defView);
         widgetFrameView.setMinimumWidth(0);
-        defView.setBackground(getContext().getDrawable(android.R.drawable.ic_menu_revert));
+        defView.setBackground(getContext().getDrawable(R.drawable.ic_settings_backup_restore));
         defView.setTag("default");
         defView.setOnClickListener(new View.OnClickListener() {
             @Override